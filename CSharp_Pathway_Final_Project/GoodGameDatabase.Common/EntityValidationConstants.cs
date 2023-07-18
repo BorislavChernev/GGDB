9b12639b6ba62b371ed693255583cbba9566553d
Borislav Chernev <borocool359@gmail.com>
2023-07-18 20:48:26 +0300

    Identity Scaffolded

--- CSharp_Pathway_Final_Project/GoodGameDatabase.Common/EntityValidationConstants.cs
+++ CSharp_Pathway_Final_Project/GoodGameDatabase.Common/EntityValidationConstants.cs
@@ -6,9 +6,11 @@
         {
             public const int NameMinLength = 2;
             public const int NameMaxLength = 20;
+            public const string NameErrorMessage = "Name length should be between 2 and 20 characters";
 
             public const int DescriptionMinLength = 30;
             public const int DescriptionMaxLength = 200;
+            public const string DescriptionErrorMessage = "Description length should be between 30 and 200 characters";
         }
     }
 }
